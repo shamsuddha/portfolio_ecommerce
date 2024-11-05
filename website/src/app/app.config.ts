b6f3b3b128317591eb1dd832bd254bbd3dcd034b
shamsuddha <shamsuddha@gmail.com>
2024-11-06 01:04:29 +0600

    dd

--- website/src/app/app.config.ts
+++ website/src/app/app.config.ts
@@ -6,5 +6,5 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync('noop')]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync('noop'), provideAnimationsAsync('noop'), provideAnimationsAsync('noop')]
 };
