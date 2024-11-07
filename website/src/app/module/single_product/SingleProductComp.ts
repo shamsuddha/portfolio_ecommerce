4e6341144b6b6938cf12be08b4fb6d93236f3682
shamsuddha <shamsuddha@gmail.com>
2024-11-07 19:23:33 +0600

    dd

--- website/src/app/module/single_product/SingleProductComp.ts
+++ website/src/app/module/single_product/SingleProductComp.ts
@@ -20,24 +20,30 @@ export class SingleProductComp implements OnInit {
 
   ngOnInit(): void { }
 
-  imageObject = [
-    {
-      image: 'https://via.placeholder.com/800x600', // Full-size image
-      thumbImage: 'https://via.placeholder.com/400x300', // Thumbnail image
-      title: 'Image 1'
-    },
-    {
-      image: 'https://via.placeholder.com/800x600',
-      thumbImage: 'https://via.placeholder.com/400x300',
-      title: 'Image 2'
-    },
-    {
-      image: 'https://via.placeholder.com/800x600',
-      thumbImage: 'https://via.placeholder.com/400x300',
-      title: 'Image 3'
-    }
-  ];
+  // Array of image URLs
+  images = [
+    'https://plus.unsplash.com/premium_photo-1674641194949-e154719cdc02?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c2xpZGVyfGVufDB8fDB8fHww',
+    'https://www.w3rack.com/wp-content/uploads/2017/05/slider-img1.jpg',
+    'https://soliloquywp.com/wp-content/uploads/2019/04/nb_esc_cover.jpg',
+];
 
+// Current selected image index
+currentIndex = 0;
+
+// Navigate to previous image
+prev() {
+    this.currentIndex = (this.currentIndex > 0) ? this.currentIndex - 1 : this.images.length - 1;
+}
+
+// Navigate to next image
+next() {
+    this.currentIndex = (this.currentIndex < this.images.length - 1) ? this.currentIndex + 1 : 0;
+}
+
+// Change the main image
+selectImage(index: number) {
+    this.currentIndex = index;
+}
   selectSize(size: string){
     this.selectedSize = size;
   }
