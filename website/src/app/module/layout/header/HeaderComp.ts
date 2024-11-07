990b8d372fed29c3b334c11bb952dea498fb0da8
shamsuddha <shamsuddha@gmail.com>
2024-11-08 00:19:55 +0600

    dd

--- website/src/app/module/layout/header/HeaderComp.ts
+++ website/src/app/module/layout/header/HeaderComp.ts
@@ -4,13 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderTopComp } from './header_top/HeaderTopComp';
 import { MenuComp } from "./menu/MenuComp";
 import { MatMenuModule } from '@angular/material/menu';
+import {MatBadgeModule} from '@angular/material/badge';
 
 @Component({
   selector: 'HeaderComp',
   templateUrl: './HeaderComp.html',
   styleUrls: ['./HeaderComp.scss'],
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatMenuModule, HeaderTopComp, MenuComp],
+  imports: [CommonModule, RouterOutlet, MatMenuModule, HeaderTopComp, MenuComp, MatBadgeModule],
   providers: [],
 })
 export class HeaderComp implements OnInit {
