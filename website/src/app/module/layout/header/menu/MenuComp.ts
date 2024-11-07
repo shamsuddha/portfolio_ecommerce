dc5b5154ec1cb2edfbf360106d2d577b82ba31ad
shamsuddha <shamsuddha@gmail.com>
2024-11-07 21:57:41 +0600

    dd

--- website/src/app/module/layout/header/menu/MenuComp.ts
+++ website/src/app/module/layout/header/menu/MenuComp.ts
@@ -3,12 +3,12 @@ import { Component, OnInit } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
- selector: 'MenuComp',
- templateUrl: './MenuComp.html',
- styleUrls: ['./MenuComp.scss'],
- standalone: true,
- imports: [CommonModule, MatMenuModule],
- providers: [],
+  selector: 'MenuComp',
+  templateUrl: './MenuComp.html',
+  styleUrls: ['./MenuComp.scss'],
+  standalone: true,
+  imports: [CommonModule, MatMenuModule],
+  providers: [],
 })
 export class MenuComp implements OnInit {
 
